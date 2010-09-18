80f6390d4e111b540b8483dd08dc0522f5693482
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-18 02:45:33 +0000

    Clearing shit out

--- Gmr190/ICA1/ICA1/Program.cs
+++ Gmr190/ICA1/ICA1/Program.cs
@@ -4,40 +4,8 @@ namespace ICA1
 {
 	class Program
 	{
-		private const double DBL_TaxMultiplier = 1.07;
 		static void Main(string[] args)
 		{
-			double Subtotal = 0.0;
-			double Total = 0.0;
-
-			while (true)
-			{
-				Console.WriteLine(ICA1.Properties.Resources.SubtotalPrompt);
-
-				try
-				{
-					Subtotal = Convert.ToDouble(Console.ReadLine());
-				}
-				catch (FormatException ex)
-				{
-					//try again
-					Console.WriteLine(ICA1.Properties.Resources.FormatExceptionMessage + ex.Message);
-					continue;
-				}
-				catch (OverflowException ex)
-				{
-					//try again
-					Console.WriteLine(ICA1.Properties.Resources.OverflowExceptionMessage + ex.Message);
-					continue;
-				}
-
-				break;
-			}
-
-			Total = Subtotal * DBL_TaxMultiplier;
-
-			Console.WriteLine(ICA1.Properties.Resources.TotalOutput, Total);
-			Console.ReadLine();
 		}
 	}
 }
