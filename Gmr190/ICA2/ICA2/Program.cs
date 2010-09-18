3e47da6fc07849e101f361a29582ec98c3f1ddf1
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-18 03:05:02 +0000

    ICA 2

---
+++ Gmr190/ICA2/ICA2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICA2
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double dHighTemperature = 0.0;
+			double dLowTemperature = 0.0;
+			double dAverage = 0.0;
+
+			try
+			{
+				Console.WriteLine("Please enter the high temperature: ");
+				dHighTemperature = Convert.ToDouble(Console.ReadLine());
+
+				Console.WriteLine("Please enter the low temperature: ");
+				dLowTemperature = Convert.ToDouble(Console.ReadLine());
+
+				dAverage = (dHighTemperature + dLowTemperature) / 2.0;
+
+				Console.WriteLine("Average: {0}, High: {1}, Low: {2}", dAverage, dHighTemperature, dLowTemperature);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Something went terribly wrong: " + e.Message);
+			}
+			finally
+			{
+				Console.ReadLine();
+			}
+		}
+	}
+}
