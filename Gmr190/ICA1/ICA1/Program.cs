76d7acb52bfe5e8c1fd10ae1ec74324bef0d2692
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-18 02:03:16 +0000

    Pointless exception handling.

--- Gmr190/ICA1/ICA1/Program.cs
+++ Gmr190/ICA1/ICA1/Program.cs
@@ -13,9 +13,26 @@ namespace ICA1
             double Total;
             string sSubtotal;
 
-            Console.WriteLine("Please enter the Subtotal: ");
-            sSubtotal = Console.ReadLine();
-            Subtotal = Convert.ToDouble(sSubtotal);
+            while (true)
+            {
+                Console.WriteLine("Please enter the Subtotal: ");
+                sSubtotal = Console.ReadLine();
+
+                try
+                {
+                    Subtotal = Convert.ToDouble(sSubtotal);
+                }
+                catch (FormatException ex)
+                {
+                    continue;
+                }
+                catch (OverflowException ex)
+                {
+                    continue;
+                }
+
+                break;
+            }
 
             Total = Subtotal * 1.07;
 
