3478c82c9ca3a426b6f0d4eca2fd5bcdf84d2753
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-18 02:53:40 +0000

    Corrected!?

--- Gmr190/ICA1/ICA1/Program.cs
+++ Gmr190/ICA1/ICA1/Program.cs
@@ -6,6 +6,30 @@ namespace ICA1
 	{
 		static void Main(string[] args)
 		{
+			double dSubtotal = 0.0;
+			double dTotal = 0.0;
+			double dGST = 0.0;
+
+			try
+			{
+				Console.WriteLine("Please enter the Subtotal: ");
+				dSubtotal = Convert.ToDouble(Console.ReadLine());
+
+				Console.WriteLine("Please enter the GST (%):");
+				dGST = Convert.ToDouble(Console.ReadLine());
+
+				dTotal = dSubtotal + (dGST / 100.0 * dSubtotal);
+
+				Console.WriteLine("Total is: {0:C}", dTotal);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine("Something went wrong! " + e.Message);
+			}
+			finally
+			{
+				Console.ReadLine();
+			}
 		}
 	}
 }
