3aaecaf6fecee671c198a21ec5c590368473f670
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-18 02:20:03 +0000

    Pointless resourceifying.

--- Gmr190/ICA1/ICA1/Program.cs
+++ Gmr190/ICA1/ICA1/Program.cs
@@ -5,40 +5,42 @@ using System.Text;
 
 namespace ICA1
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            double Subtotal;
-            double Total;
-            string sSubtotal;
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			double Subtotal;
+			double Total;
 
-            while (true)
-            {
-                Console.WriteLine("Please enter the Subtotal: ");
-                sSubtotal = Console.ReadLine();
+			while (true)
+			{
+				Console.WriteLine(ICA1.Properties.Resources.SubtotalPrompt);
 
-                try
-                {
-                    Subtotal = Convert.ToDouble(sSubtotal);
-                }
-                catch (FormatException ex)
-                {
-                    continue;
-                }
-                catch (OverflowException ex)
-                {
-                    continue;
-                }
+				try
+				{
+					Subtotal = Convert.ToDouble(Console.ReadLine());
+				}
+				catch (FormatException ex)
+				{
+					//try again
+					Console.WriteLine(ICA1.Properties.Resources.FormatExceptionMessage + ex.Message);
+					continue;
+				}
+				catch (OverflowException ex)
+				{
+					//try again
+					Console.WriteLine(ICA1.Properties.Resources.OverflowExceptionMessage + ex.Message);
+					continue;
+				}
 
-                break;
-            }
+				break;
+			}
 
-            Total = Subtotal * 1.07;
+			Total = Subtotal * 1.07;
 
-            Console.WriteLine("Total: {0}", Total);
+			Console.WriteLine(ICA1.Properties.Resources.TotalOutput, Total.ToString());
 
-            Console.ReadLine();
-        }
-    }
+			Console.ReadLine();
+		}
+	}
 }
