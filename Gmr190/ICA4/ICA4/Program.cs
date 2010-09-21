31dfaa84683803eebc936ee2b5eb216e21467b21
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-21 03:05:54 +0000

    Simple demo of if/else

---
+++ Gmr190/ICA4/ICA4/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICA4
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			string name;
+			string me = "ADDISON";
+
+			Console.Write("Please enter your name: ");
+			name = Console.ReadLine();
+
+			if (name.ToUpper() == me)
+			{
+				Console.WriteLine("Hello me!");
+			}
+			else
+			{
+				Console.WriteLine("Hello someone else.");
+			}
+
+			Console.ReadKey();
+		}
+	}
+}
