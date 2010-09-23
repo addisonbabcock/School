8f29aeca3abb5d220716fe9ea86c0ff7c6cf91ba
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-23 02:49:29 +0000

    Moved the try block a bit. Apparently Console.ReadLine () and Console.Write () do throw exceptions....

--- Gmr190/RealLifeCoding/RealLifeCoding/Program.cs
+++ Gmr190/RealLifeCoding/RealLifeCoding/Program.cs
@@ -13,25 +13,25 @@ namespace RealLifeCoding
 	{
 		static void Main (string [] args)
 		{
-			Console.WriteLine ("Welcome to NAIT course registration system!");
-
-			Console.Write ("Please enter course ID: ");
-			string courseID = Console.ReadLine ();
-
-			Console.Write ("Please enter course name: ");
-			string courseName = Console.ReadLine ();
-
-			Console.Write ("Please enter the room number: ");
-			string room = Console.ReadLine ();
-
-			Console.Write ("Please enter the number of students: ");
-			string studentCount = Console.ReadLine ();
-
-			Console.Write ("Please enter the name of the instructor: ");
-			string instructor = Console.ReadLine ();
-
 			try
 			{
+				Console.WriteLine ("Welcome to NAIT course registration system!");
+
+				Console.Write ("Please enter course ID: ");
+				string courseID = Console.ReadLine ();
+
+				Console.Write ("Please enter course name: ");
+				string courseName = Console.ReadLine ();
+
+				Console.Write ("Please enter the room number: ");
+				string room = Console.ReadLine ();
+
+				Console.Write ("Please enter the number of students: ");
+				string studentCount = Console.ReadLine ();
+
+				Console.Write ("Please enter the name of the instructor: ");
+				string instructor = Console.ReadLine ();
+
 				int nStudentCount = Convert.ToInt32 (studentCount);
 
 				Course my101course = new Course (
