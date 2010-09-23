0eb6c8ef077584bd237f21baef9b22885e53667f
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-23 02:55:44 +0000

    Pretty simple...

---
+++ Gmr190/ICA5/ICA5/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICA5
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			try
+			{
+				Console.WriteLine ("GMR 190 number comparator!");
+
+				int high = 0;
+				int num1 = 0;
+				int num2 = 0;
+
+				Console.Write ("Please input number 1: ");
+				string sNum1 = Console.ReadLine ();
+
+				Console.Write ("Please input number 2: ");
+				string sNum2 = Console.ReadLine ();
+
+				num1 = Convert.ToInt32 (sNum1);
+				num2 = Convert.ToInt32 (sNum2);
+
+				if (num1 > num2)
+				{
+					high = num1;
+				}
+				else
+				{
+					high = num2;
+				}
+
+				Console.Write ("The highest number is: {0}\n", high);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine ("Failed at something: {0}", e.Message);
+			}
+			finally
+			{
+				Console.ReadLine ();
+			}
+		}
+	}
+}
