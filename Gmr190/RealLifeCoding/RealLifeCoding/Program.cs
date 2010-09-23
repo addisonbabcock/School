d5819ccfdf3a83c0b65f28d7f0f64377aded0656
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-23 02:46:26 +0000

    Interactivity!

--- Gmr190/RealLifeCoding/RealLifeCoding/Program.cs
+++ Gmr190/RealLifeCoding/RealLifeCoding/Program.cs
@@ -13,18 +13,55 @@ namespace RealLifeCoding
 	{
 		static void Main (string [] args)
 		{
-			Course my101course = new Course ("GMR101", "Intro", "WB306", 11, "Jay");
+			Console.WriteLine ("Welcome to NAIT course registration system!");
 
-			Console.WriteLine (my101course.ToString ());
-			Console.WriteLine ();
+			Console.Write ("Please enter course ID: ");
+			string courseID = Console.ReadLine ();
 
-			my101course.SetClassroom ("WB312");
+			Console.Write ("Please enter course name: ");
+			string courseName = Console.ReadLine ();
+
+			Console.Write ("Please enter the room number: ");
+			string room = Console.ReadLine ();
+
+			Console.Write ("Please enter the number of students: ");
+			string studentCount = Console.ReadLine ();
+
+			Console.Write ("Please enter the name of the instructor: ");
+			string instructor = Console.ReadLine ();
+
+			try
+			{
+				int nStudentCount = Convert.ToInt32 (studentCount);
+
+				Course my101course = new Course (
+					courseID,
+					courseName,
+					room,
+					Convert.ToInt32 (studentCount),
+					instructor);
+
+				Console.WriteLine ();
+				Console.WriteLine ("Process complete!");
+				Console.WriteLine (my101course.ToString ());
+				Console.WriteLine ();
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine ("Failed to convert input to integer: " + e.Message);
+			}
+			finally
+			{
+				Console.ReadKey ();
+			}
+
+/*			my101course.SetClassroom ("WB312");
 			my101course.SetStudentCount (22);
 			my101course.SetInstructor ("Someone");
 
 			Console.WriteLine (my101course.ToString ());
 
-			Console.ReadKey ();
+			Console.ReadKey ();*/
 		}
 	}
 }
