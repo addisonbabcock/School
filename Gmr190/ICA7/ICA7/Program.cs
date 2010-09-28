3dd962c4677ee49bf40998a2197ef4d6e88002e3
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-28 02:34:40 +0000

    Calculating factorials.

---
+++ Gmr190/ICA7/ICA7/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICA7
+{
+	class Program
+	{
+		public static int GrabInt (string message)
+		{
+			int ret = 0;
+			while (true)
+			{
+				try
+				{
+					Console.Write (message);
+					ret = Convert.ToInt32 (Console.ReadLine ());
+				}
+				catch (Exception e)
+				{
+					Console.WriteLine ("Invalid input: " + e.Message);
+					continue;
+				}
+
+				return ret;
+			}
+		}
+
+		static void Main (string [] args)
+		{
+			int factorial = 0;
+			int number = 0;
+			int origNumber = 0;
+
+			factorial = 1;
+
+			origNumber = number = GrabInt ("Please enter the positive number you want to see the factorial of: ");
+
+			while (number > 1)
+			{
+				factorial = factorial * number;
+				number = number - 1;
+			}
+
+			Console.WriteLine ("The factorial of {0} is {1}", origNumber, factorial);
+			Console.ReadKey ();
+		}
+	}
+}
