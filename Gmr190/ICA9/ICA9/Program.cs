4af35bfa26eb52bd91788e1d2768f6fbf3aa0d7b
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-30 01:48:14 +0000

    Simple do while looping

---
+++ Gmr190/ICA9/ICA9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ICA9
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			int count = 1;
+
+			try
+			{
+				Console.Write ("Please enter how many times to loop: 9");
+				count = Convert.ToInt32 (Console.ReadLine ());
+
+				do
+				{
+					Console.WriteLine (count);
+					--count;
+				} while (count > 0);
+			}
+			catch (Exception e)
+			{
+				Console.WriteLine (e.Message);
+			}
+
+			Console.ReadKey ();
+		}
+	}
+}
