a3dd10c46123c21d89991281c715be1e4698fd64
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-09-30 02:12:07 +0000

    hax

---
+++ Gmr190/do_while_exercise2/do_while_exercise2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace do_while_exercise2
+{
+	class Program
+	{
+		static void Main (string [] args)
+		{
+			int count = 0;
+			int highTotal = 0;
+			int numDays = 0;
+			int dailyHigh = 0;
+			int averageHigh = 0;
+			string monthName = "";
+
+			Console.Write ("Please enter the month: ");
+			monthName = Console.ReadLine ();
+
+			Console.Write ("Please enter the number of days: ");
+			numDays = Convert.ToInt32 (Console.ReadLine ());
+
+			do
+			{
+				Console.Write ("Please enter the daily high temperature: ");
+				dailyHigh = Convert.ToInt32 (Console.ReadLine ());
+				highTotal += dailyHigh;
+				++count;
+			} while (count < numDays);
+
+			averageHigh = highTotal / numDays;
+			Console.Write ("The average high temperature for the month of {0} was {1}", monthName, averageHigh);
+			Console.ReadKey ();
+		}
+	}
+}
