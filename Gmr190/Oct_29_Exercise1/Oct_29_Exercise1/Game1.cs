f80be7e2383ec5a00161f28d8b5beabed0dfaaee
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-10-30 02:17:41 +0000

    Spin!

---
+++ Gmr190/Oct_29_Exercise1/Oct_29_Exercise1/Game1.cs
@@ -0,0 +1,120 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.GamerServices;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Media;
+using Microsoft.Xna.Framework.Net;
+using Microsoft.Xna.Framework.Storage;
+
+namespace Oct_29_Exercise1
+{
+    /// <summary>
+    /// This is the main type for your game
+    /// </summary>
+    public class Game1 : Microsoft.Xna.Framework.Game
+    {
+        GraphicsDeviceManager   graphics;
+        SpriteBatch             spriteBatch;
+        List<Sprite>            mSprites;
+
+        Texture2D               mCat;
+        Texture2D               mDog;
+
+        public Game1()
+        {
+            graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+        }
+
+        /// <summary>
+        /// Allows the game to perform any initialization it needs to before starting to run.
+        /// This is where it can query for any required services and load any non-graphic
+        /// related content.  Calling base.Initialize will enumerate through any components
+        /// and initialize them as well.
+        /// </summary>
+        protected override void Initialize()
+        {
+            base.Initialize();
+
+            mSprites = new List<Sprite>();
+
+            Vector2 catPos = new Vector2(
+                graphics.GraphicsDevice.Viewport.Width / 2, 
+                graphics.GraphicsDevice.Viewport.Height / 2);
+            Vector2 catSpeed = new Vector2(0.5f, 0.0f);
+            double catRotSpeed = 0.005;
+            mSprites.Add(new Sprite(mCat, catPos, catSpeed, catRotSpeed, true, graphics));
+
+            for (int i = 0; i < 10; ++i)
+            {
+                Vector2 dogPos = new Vector2(
+                    i * -1000,
+                    graphics.GraphicsDevice.Viewport.Height / 2);
+                Vector2 dogSpeed = new Vector2(1.0f, 0.0f);
+                double dogRotSpeed = -0.0025;
+                mSprites.Add(new Sprite(mDog, dogPos, dogSpeed, dogRotSpeed, false, graphics));
+            }
+        }
+
+        /// <summary>
+        /// LoadContent will be called once per game and is the place to load
+        /// all of your content.
+        /// </summary>
+        protected override void LoadContent()
+        {
+            // Create a new SpriteBatch, which can be used to draw textures.
+            spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            mCat = Content.Load<Texture2D>("cats_012");
+            mDog = Content.Load<Texture2D>("worlds-strongest-dog");
+        }
+
+        /// <summary>
+        /// UnloadContent will be called once per game and is the place to unload
+        /// all content.
+        /// </summary>
+        protected override void UnloadContent()
+        {
+            // TODO: Unload any non ContentManager content here
+        }
+
+        /// <summary>
+        /// Allows the game to run logic such as updating the world,
+        /// checking for collisions, gathering input, and playing audio.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Update(GameTime gameTime)
+        {
+            // Allows the game to exit
+            if (GamePad.GetState(PlayerIndex.One).Buttons.Back == ButtonState.Pressed ||
+                Keyboard.GetState ().IsKeyDown (Keys.Escape))
+                this.Exit();
+
+            base.Update(gameTime);
+
+            foreach (Sprite sprite in mSprites)
+                sprite.Update(gameTime);
+        }
+
+        /// <summary>
+        /// This is called when the game should draw itself.
+        /// </summary>
+        /// <param name="gameTime">Provides a snapshot of timing values.</param>
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.CornflowerBlue);
+
+            spriteBatch.Begin();
+            foreach (Sprite sprite in mSprites)
+                sprite.Draw(spriteBatch);
+            spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+    }
+}
---
+++ Gmr190/Oct_29_Exercise1/Oct_29_Exercise1/Sprite.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Oct_29_Exercise1
+{
+    class Sprite
+    {
+        private Texture2D mTexture;
+        private Vector2 mPos;
+        private double mRot;
+        private Vector2 mSpeed; //pixels per millisecond
+        private double mSpinSpeed; //radians per millisecond
+        private bool mWrapPosition;
+        private GraphicsDeviceManager mGraphics;
+
+        public Sprite(
+            Texture2D texture, 
+            Vector2 pos, 
+            Vector2 speed, 
+            double spinSpeed, 
+            bool wrapPosition, 
+            GraphicsDeviceManager graphics)
+        {
+            mTexture = texture;
+            mPos = pos;
+            mRot = 0.0;
+            mSpeed = speed;
+            mSpinSpeed = spinSpeed;
+            mWrapPosition = wrapPosition;
+            mGraphics = graphics;
+        }
+
+        public void Update(GameTime time)
+        {
+            mPos.X += mSpeed.X * (float)time.ElapsedGameTime.TotalMilliseconds;
+            mPos.Y += mSpeed.Y * (float)time.ElapsedGameTime.TotalMilliseconds;
+            mRot += mSpinSpeed * time.ElapsedGameTime.TotalMilliseconds;
+
+            if (mWrapPosition)
+            {
+                while (mPos.X >= mGraphics.GraphicsDevice.Viewport.Width)
+                    mPos.X -= mGraphics.GraphicsDevice.Viewport.Width;
+                while (mPos.X < 0)
+                    mPos.X += mGraphics.GraphicsDevice.Viewport.Width;
+                while (mPos.Y >= mGraphics.GraphicsDevice.Viewport.Height)
+                    mPos.Y -= mGraphics.GraphicsDevice.Viewport.Height;
+                while (mPos.Y < 0)
+                    mPos.Y += mGraphics.GraphicsDevice.Viewport.Height;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(
+                mTexture, 
+                mPos, 
+                new Rectangle (0, 0, mTexture.Width, mTexture.Height), 
+                Color.White, 
+                (float)mRot, 
+                new Vector2(
+                    mTexture.Width / 2,
+                    mTexture.Height / 2),
+                1.0f,
+                SpriteEffects.None, 
+                0.0f);
+        }
+    }
+}
