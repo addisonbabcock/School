079682008b06e79add4eaeb527fa03e1539c760e
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2010-12-09 17:25:44 +0000

    Set up the setup... cough cough

--- Gmr190/Rejeweled/Rejeweled/Properties/AssemblyInfo.cs
+++ Gmr190/Rejeweled/Rejeweled/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Resources;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -30,4 +31,5 @@ using System.Runtime.InteropServices;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: NeutralResourcesLanguageAttribute("en-CA")]
