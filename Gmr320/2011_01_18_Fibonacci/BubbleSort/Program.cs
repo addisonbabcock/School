f31a72ba2c0ae1f075c8b8f53d59c943631580be
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2011-01-19 04:08:36 +0000

    added bubblesort demo

---
+++ Gmr320/2011_01_18_Fibonacci/BubbleSort/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BubbleSort
+{
+	class Program
+	{
+		static Random rng;
+
+		static void BubbleSort (List<int> arr)
+		{
+			for (int pass = 1; pass < arr.Count; ++pass)
+			{
+				bool didSwap = false;
+
+				for (int i = 0; i < arr.Count - pass; ++i)
+				{
+					if (arr [i] > arr [i + 1])
+					{
+						int temp = arr [i];
+						arr [i] = arr [i + 1];
+						arr [i + 1] = temp;
+						didSwap = true;
+					}
+				}
+
+				if (!didSwap)
+					return;
+			}
+		}
+
+		static void Main (string [] args)
+		{
+			rng = new Random ();
+			List<int> arr = new List<int> ();
+			for (int i = 0; i < 15; ++i)
+			{
+				arr.Add (rng.Next (100));
+			}
+
+			BubbleSort (arr);
+
+			foreach (int i in arr)
+			{
+				Console.Write ("{0} ", i);
+			}
+			Console.ReadKey ();
+		}
+	}
+}
