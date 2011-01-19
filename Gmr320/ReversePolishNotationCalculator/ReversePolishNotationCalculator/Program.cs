76c8ea2cca561fc3e360ad0fc245419c074469d5
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2011-01-19 21:14:09 +0000

    done?

--- Gmr320/ReversePolishNotationCalculator/ReversePolishNotationCalculator/Program.cs
+++ Gmr320/ReversePolishNotationCalculator/ReversePolishNotationCalculator/Program.cs
@@ -9,6 +9,55 @@ namespace ReversePolishNotationCalculator
 	{
 		static void Main (string [] args)
 		{
+			double result = 0.0;
+			Stack<double> numbers = new Stack<double> ();
+			char [] splitters = {' '};
+
+			Console.WriteLine ("What would you like me to calculate? ");
+			string input = Console.ReadLine ();
+
+			List<string> tokens = input.Split (splitters).ToList<string> ();
+
+			foreach (string token in tokens)
+			{
+				try
+				{
+					double number = Convert.ToDouble (token);
+
+					if (numbers.Count == 0)
+						result = number;
+
+					numbers.Push (number);
+				}
+				catch (FormatException)
+				{
+					switch (token)
+					{
+						case "+":
+							result += numbers.Pop ();
+							break;
+
+						case "-":
+							result -= numbers.Pop ();
+							break;
+
+						case "*":
+							result *= numbers.Pop ();
+							break;
+
+						case "/":
+							result /= numbers.Pop ();
+							break;
+
+						default:
+							Console.WriteLine ("I don't know what {0} means.", token);
+							break;
+					}
+				}
+			}
+
+			Console.WriteLine ("Result: {0}", result);
+			Console.ReadKey ();
 		}
 	}
 }
