14db926feb3bf33a714a4c83fc5ee1029516765d
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2011-01-28 02:40:58 +0000

    Added factorial calculator

---
+++ Gmr320/ReversePolishNotationCalculator/FactorialCalulator/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FactorialCalulator
+{
+	class Program
+	{
+		static UInt64 Factorial (UInt64 a)
+		{
+			if (a == 0)
+				return 1;
+
+			return Factorial (a - 1) * a;
+		}
+
+		static void Main (string [] args)
+		{
+			for (int i = 0; i < 10; ++i)
+				Console.WriteLine ("{0} - {1}", i, Factorial ((UInt64)i));
+			Console.ReadKey ();
+		}
+	}
+}
