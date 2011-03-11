fd8141d39e3e9ce72d12a6cfcfe81605aa67262a
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2011-03-11 03:25:30 +0000

    Works like shit. Don't do this IRL.

--- Gmr320/GMR320_Assignment3/GMR320_Assignment3_Question1/Program.cs
+++ Gmr320/GMR320_Assignment3/GMR320_Assignment3_Question1/Program.cs
@@ -9,6 +9,20 @@ namespace GMR320_Assignment3_Question1
 	{
 		static void Main (string [] args)
 		{
+			double x = 1.0;
+			double y = 1.0;
+
+			for (int i = 0; i < 360; ++i)
+			{
+				double newX = x * Math.Cos (1.0) - y * Math.Sin (1.0);
+				double newY = x * Math.Sin (1.0) + y * Math.Cos (1.0);
+
+				x = newX;
+				y = newY;
+			}
+
+			Console.WriteLine ("Resulting point: {0}, {1}", x, y);
+			Console.ReadLine ();
 		}
 	}
 }
