fc0a084f0ccc009415761e8fb846e915ce278488
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2011-03-11 03:37:27 +0000

    Sigh at radians.

--- Gmr320/GMR320_Assignment3/GMR320_Assignment3_Question1/Program.cs
+++ Gmr320/GMR320_Assignment3/GMR320_Assignment3_Question1/Program.cs
@@ -11,11 +11,12 @@ namespace GMR320_Assignment3_Question1
 		{
 			double x = 1.0;
 			double y = 1.0;
+			double angle = Math.PI / 180.0;
 
 			for (int i = 0; i < 360; ++i)
 			{
-				double newX = x * Math.Cos (1.0) - y * Math.Sin (1.0);
-				double newY = x * Math.Sin (1.0) + y * Math.Cos (1.0);
+				double newX = x * Math.Cos (angle) - y * Math.Sin (angle);
+				double newY = x * Math.Sin (angle) + y * Math.Cos (angle);
 
 				x = newX;
 				y = newY;
@@ -23,6 +24,9 @@ namespace GMR320_Assignment3_Question1
 
 			Console.WriteLine ("Resulting point: {0}, {1}", x, y);
 			Console.ReadLine ();
+
+			//Don't do it this way!
+			//Instead start each frame at 1.0, 1.0 then rotate by 1.0, 2.0, 3.0, etc
 		}
 	}
 }
