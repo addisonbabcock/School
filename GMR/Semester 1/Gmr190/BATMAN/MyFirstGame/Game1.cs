9ed2e6c6cbf2b208a6e8726532044ccf661d7192
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2012-09-27 19:43:25 +0000

    Update to VS 2012. --Addison

--- GMR/Semester 1/Gmr190/BATMAN/MyFirstGame/Game1.cs
+++ GMR/Semester 1/Gmr190/BATMAN/MyFirstGame/Game1.cs
@@ -1,15 +1,8 @@
 using System;
 using System.Collections.Generic;
-using System.Linq;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
 
 namespace BATMAN
 {
@@ -42,7 +35,7 @@ namespace BATMAN
 			Content.RootDirectory = "Content";
 
 			Window.AllowUserResizing = false;
-			Window.ClientSizeChanged += new EventHandler (Window_ClientSizeChanged);
+			Window.ClientSizeChanged += new EventHandler <EventArgs> (Window_ClientSizeChanged);
 
 			myBackground_Rect = new Rectangle (
 				0,
