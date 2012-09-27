a74c1ea10437984fac11924f1ace03485f0f78c0
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2012-09-27 21:08:17 +0000

    Update to VS 2010. --Addison

---
+++ GMR/Semester 1/Gmr190/Oct_22_Exercise4/Utilities/ConsoleUtils.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Utilities
+{
+    public static class ConsoleUtils
+    {
+        public static int GetInt(string message, int min, int max)
+        {
+            int returnVal = min;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                try
+                {
+                    returnVal = Convert.ToInt32(Console.ReadLine());
+
+                    if (returnVal >= min && returnVal <= max)
+                        return returnVal;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unacceptable input. " + e.Message);
+                }
+            }
+        }
+
+        public static int GetInt(string message)
+        {
+            return GetInt(message, int.MinValue, int.MaxValue);
+        }
+
+        public static double GetDouble(string message, double min, double max)
+        {
+            double returnVal = min;
+
+            while (true)
+            {
+                Console.Write(message);
+
+                try
+                {
+                    returnVal = Convert.ToDouble(Console.ReadLine());
+
+                    if (returnVal >= min && returnVal <= max)
+                        return returnVal;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Unacceptable input. " + e.Message);
+                }
+            }
+        }
+
+        public static double GetDouble(string message)
+        {
+            return GetDouble(message, double.MinValue, double.MaxValue);
+        }
+    }
+}
