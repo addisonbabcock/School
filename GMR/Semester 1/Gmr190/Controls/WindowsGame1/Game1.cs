4e40f5f3c47b4713f19a095e13c8c410f65e6bb3
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2012-09-27 19:47:51 +0000

    Update to VS 2012. --Addison

--- GMR/Semester 1/Gmr190/Controls/WindowsGame1/Game1.cs
+++ GMR/Semester 1/Gmr190/Controls/WindowsGame1/Game1.cs
@@ -1,16 +1,6 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Diagnostics;
 using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Audio;
-using Microsoft.Xna.Framework.Content;
-using Microsoft.Xna.Framework.GamerServices;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
-using Microsoft.Xna.Framework.Media;
-using Microsoft.Xna.Framework.Net;
-using Microsoft.Xna.Framework.Storage;
 
 namespace Controls
 {
@@ -55,7 +45,7 @@ namespace Controls
             spriteBatch = new SpriteBatch(GraphicsDevice);
 
             mSquiggle = Content.Load<Texture2D>("SquiggleThing");
-            mSquiggle.GenerateMipMaps(TextureFilter.Anisotropic);
+            //mSquiggle.GenerateMipMaps(TextureFilter.Anisotropic);
         }
 
         /// <summary>
