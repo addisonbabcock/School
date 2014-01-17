ff5eae3cfc326eeb9fe714c8b4d48cb97ef39bf7
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2014-01-17 01:51:03 +0000

    Adding TimeServer and TimeClient. January 16 2014.

---
+++ BAIST/BAIS3110/TimeClient/TimeClient/TimeClient.vb
@@ -0,0 +1,50 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.IO
+
+Module TimeClient
+
+    Sub Main()
+        Dim tcpc As New TcpClient()
+        Dim read(35) As Byte
+        Dim args As String() = Environment.GetCommandLineArgs()
+
+        If (args.Length < 2) Then
+            Console.WriteLine("Please specify a server name in the command line.")
+            Exit Sub
+        End If
+
+        Dim server As String = args(1)
+
+        Try
+            Dns.GetHostEntry(server)
+        Catch
+            Console.WriteLine("Cannot find server: {0}", server)
+            Exit Sub
+        End Try
+
+        tcpc.Connect(server, 14)
+
+        Dim s As Stream
+        Try
+            s = tcpc.GetStream()
+        Catch exc As InvalidOperationException
+            Console.WriteLine("Cannot connect to server: {0}", server)
+            Exit Sub
+        End Try
+
+        Dim bytes As Integer = s.Read(read, 0, read.Length)
+        Dim time As String = Encoding.ASCII.GetString(read)
+
+        Console.WriteLine("Received {0} bytes", bytes)
+        Console.WriteLine("Current date and time is: {0}", time)
+
+        tcpc.Close()
+
+        Console.WriteLine("Press return to exit")
+        Console.Read()
+    End Sub
+
+End Module
---
+++ BAIST/BAIS3110/TimeServer/TimeServer/TimeServer.vb
@@ -0,0 +1,43 @@
+﻿Imports System
+Imports System.Net
+Imports System.Net.Sockets
+Imports System.Text
+Imports System.Threading
+Imports System.Globalization
+
+Module TimeServer
+
+    Sub Main()
+        Dim now As Date
+        Dim strDateLine As String
+        Dim ASCII As Encoding = Encoding.ASCII
+
+        Thread.CurrentThread.CurrentCulture = CultureInfo.InvariantCulture
+
+        Try
+            Dim tcpl As New TcpListener(IPAddress.Any, 14)
+
+            tcpl.Start()
+
+            Console.WriteLine("Waiting for clients to connect")
+            Console.WriteLine("Press ctrl+c to quit...")
+
+            While (True)
+                Dim s As Socket = tcpl.AcceptSocket()
+
+                now = DateTime.Now
+                strDateLine = now.ToShortDateString() + " " + now.ToLongTimeString()
+
+                Dim byteDateLine() As Byte = ASCII.GetBytes(strDateLine.ToCharArray())
+                s.Send(byteDateLine, byteDateLine.Length, SocketFlags.None)
+                s.Close()
+                Console.WriteLine("Sent {0}", strDateLine)
+            End While
+        Catch socketError As SocketException
+            If (socketError.ErrorCode) = 10048 Then
+                Console.WriteLine("Connection to this port failed. There is another server listening on this port.")
+            End If
+        End Try
+    End Sub
+
+End Module
