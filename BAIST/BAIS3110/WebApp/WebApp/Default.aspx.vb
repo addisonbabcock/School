696e08a8abad10d35d106cc7c8166e33ddaae1fd
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2014-01-24 02:02:44 +0000

    Assignment 2

---
+++ BAIST/BAIS3110/WebApp/WebApp/Default.aspx.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Collections.Specialized
+
+Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim loop1, loop2 As Integer
+        Dim arr1(), arr2() As String
+        Dim col1 As NameValueCollection
+
+        col1 = Request.ServerVariables
+        arr1 = col1.AllKeys
+        For loop1 = 0 To arr1.GetUpperBound(0)
+            Response.Write("Key: " & arr1(loop1) & "<br>")
+            arr2 = col1.GetValues(loop1)
+            For loop2 = 0 To arr2.GetUpperBound(0)
+                Response.Write("Value " & CStr(loop2) & ": " & Server.HtmlEncode(arr2(loop2)) & "<br>")
+            Next
+        Next
+    End Sub
+
+End Class
