c1851be88b528d1c27e73dd701ac6d610dcdb45c
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2014-02-07 02:30:39 +0000

    February 6, 2014. ViewState fuckery.

---
+++ BAIST/BAIS3110/ViewState2010/ViewState2010/HostEnvironment.aspx.vb
@@ -0,0 +1,16 @@
+﻿Imports System.Web.Hosting
+
+Public Class HostEnvironment
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Response.Write("MaxConcurrentRequestsPerCPU: " + HostingEnvironment.MaxConcurrentRequestsPerCPU.ToString() + " <br />")
+        Response.Write("MaxConcurrentThreadsPerCPU: " + HostingEnvironment.MaxConcurrentThreadsPerCPU.ToString() + " <br />")
+        Response.Write("ApplicationHost: " + HostingEnvironment.ApplicationHost.ToString() + " <br />")
+        Response.Write("ApplicationPhysicalPath: " + HostingEnvironment.ApplicationPhysicalPath.ToString() + " <br />")
+        Response.Write("Cache: " + HostingEnvironment.Cache.ToString() + " <br />")
+
+    End Sub
+
+End Class
---
+++ BAIST/BAIS3110/ViewState2010/ViewState2010/ViewState.aspx.vb
@@ -0,0 +1,29 @@
+﻿Public Class ViewState
+    Inherits System.Web.UI.Page
+
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        If Not IsPostBack Then
+            Dim values As ArrayList = New ArrayList()
+
+            values.Add("Chips")
+            values.Add("Chocolate Bars")
+            values.Add("Popcorn")
+
+            food.DataSource = values
+            food.DataBind()
+        Else
+            ViewState("PostBackCounter") += 1
+        End If
+
+    End Sub
+
+    Protected Sub Choose_Click(ByVal sender As Object, ByVal e As System.EventArgs) Handles Choose.Click
+        Dim counter As Integer = (ViewState("PostBackCounter"))
+
+        Response.Write("You chose " + food.SelectedItem.Text + "<br />")
+        Response.Write("The button has been clicked " + counter.ToString() + " times. <br />")
+    End Sub
+
+End Class
