49c0078c2add738dac4c216be3ae983946a73e84
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2014-03-14 01:29:43 +0000

    Added another assignment.

---
+++ BAIST/BAIS3110/UserIDInputValidation/UserIDInputValidation/Default.aspx
@@ -0,0 +1,22 @@
+﻿<%@ Page Language="VB" AutoEventWireup="true" CodeFile="~/Default.aspx.vb" Inherits="_Default" ValidateRequest="false" %>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+    <head runat="server">
+        <title>Untitled Page</title>
+    </head>
+    <body>
+        <form id="form1" runat="server">
+            <div>
+                <hr />
+                HTML Input Validation Test<br />
+                <asp:TextBox ID="msg" runat="server" Rows="10" TextMode="MultiLine" Width="100%"></asp:TextBox>
+                <asp:Button ID="Button1" runat="server" Text="Submit" UseSubmitBehavior="true" /><br />
+                <hr />
+            </div>
+
+            Save to Filename:<br />
+            <asp:TextBox ID="aFilename" runat="server" Width="150"></asp:TextBox>
+            <br />
+        </form>
+    </body>
+</html>
---
+++ BAIST/BAIS3110/UserIDInputValidation/UserIDInputValidation/Default.aspx.vb
@@ -0,0 +1,75 @@
+﻿Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Function ValidateQueryString() As Boolean
+        Dim nameString As String
+        nameString = Request.QueryString("Name")
+        If String.IsNullOrEmpty(nameString) = False Then
+            If System.Text.RegularExpressions.Regex.IsMatch(nameString, "^[a-zA-Z'.\\s]{1,40}$") = False Then
+                Response.Write("Invalid query string")
+                Return False
+            End If
+        End If
+
+        Return True
+    End Function
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        If ValidateQueryString() = False Then
+            Return
+        End If
+
+        Dim fileControl As TextBox
+        fileControl = CType(form1.FindControl("aFilename"), TextBox)
+        If ValidateFilePath(fileControl.Text) = False Then
+            Response.Write("File path failed validation")
+            Return
+        End If
+
+        Response.Write("Hello ")
+        Response.Write(Request.QueryString("name"))
+        Response.Write("<br /><br />")
+        Response.Write("Message=<br />")
+        Response.Write(ValidateMessage())
+        Response.Write("<br />")
+
+    End Sub
+
+    Protected Function ValidateMessage() As String
+
+        Dim messageControl As TextBox
+        messageControl = CType(form1.FindControl("msg"), TextBox)
+
+        Dim sb As StringBuilder
+        sb = New StringBuilder(HttpUtility.HtmlEncode(messageControl.Text))
+
+        sb.Replace("&lt;b&gt;", "<b>")
+        sb.Replace("&lt;/b&gt;", "</b>")
+        sb.Replace("&lt;i&gt;", "<i>")
+        sb.Replace("&lt;/i&gt;", "</i>")
+        sb.Replace("&lt;u&gt;", "<u>")
+        sb.Replace("&lt;/u&gt;", "</u>")
+
+        sb.Replace("&lt;B&gt;", "<B>")
+        sb.Replace("&lt;/B&gt;", "</B>")
+        sb.Replace("&lt;I&gt;", "<I>")
+        sb.Replace("&lt;/I&gt;", "</I>")
+        sb.Replace("&lt;U&gt;", "<U>")
+        sb.Replace("&lt;/U&gt;", "</U>")
+
+        Return sb.ToString()
+
+        'Return messageControl.Text
+    End Function
+
+    Protected Function ValidateFilePath(ByVal filepath As String) As Boolean
+        Try
+            Request.MapPath(filepath, Request.ApplicationPath, False)
+        Catch ex As HttpException
+            Return False
+        End Try
+
+        Return True
+    End Function
+End Class
