736eb36bcd493d782028470168e55688cd5c2505
addison <addison@a587bc9e-4a24-0a48-8c52-e76df6254a9c>
2014-04-09 01:42:37 +0000

    Added encryption assignment.

---
+++ BAIST/BAIS3110/ProtectedData/ProtectedData/DPAPI.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Security.Cryptography;
+
+namespace ProtectedData
+{
+	class DPAPI
+	{
+		public byte[] Protect(byte[] data, byte[] s_additionalEntropy)
+		{
+			try
+			{
+				// Encrypt the data using DataProtectionScope.CurrentUser. The result can be decrypted 
+				//  only by the same current user. 
+				return System.Security.Cryptography.ProtectedData.Protect(data, s_additionalEntropy, DataProtectionScope.CurrentUser);
+			}
+			catch (CryptographicException e)
+			{
+				Console.WriteLine("Data was not encrypted. An error occurred.");
+				Console.WriteLine(e.ToString());
+				return null;
+			}
+		}
+
+		public byte[] Unprotect(byte[] data, byte[] s_additionalEntropy)
+		{
+			try
+			{
+				//Decrypt the data using DataProtectionScope.CurrentUser. 
+				return System.Security.Cryptography.ProtectedData.Unprotect(data, s_additionalEntropy, DataProtectionScope.CurrentUser);
+			}
+			catch (CryptographicException e)
+			{
+				Console.WriteLine("Data was not decrypted. An error occurred.");
+				Console.WriteLine(e.ToString());
+				return null;
+			}
+		}
+
+		public string EncryptString(byte[] secret, byte[] s_additionalEntropy)
+		{
+			var encrypted = Protect(secret, s_additionalEntropy);
+			return System.Convert.ToBase64String(encrypted);
+		}
+
+		internal byte[] DecryptString(string encryptedBase64, byte[] s_additionalEntropy)
+		{
+			var encrypted = System.Convert.FromBase64String(encryptedBase64);
+			return Unprotect(encrypted, s_additionalEntropy);
+		}
+	}
+}
---
+++ BAIST/BAIS3110/ProtectedData/ProtectedData/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace ProtectedData
+{
+	public class DataProtectionSample
+	{
+		// Create byte array for additional entropy when using Protect method. 
+		static byte[] s_additionalEntropy = { 9, 8, 7, 6, 5 };
+
+		public static void Main()
+		{
+			// Create a simple byte array containing data to be encrypted. 
+
+			byte[] secret = { 0, 1, 2, 3, 4, 1, 2, 3, 4 };
+
+			DPAPI dpapi = new DPAPI();
+
+			Step4(secret, dpapi);
+
+			Step5(secret, dpapi);
+
+			Step6();
+
+			Console.ReadLine();
+		}
+
+		private static void Step6()
+		{
+			byte[] numbers = new byte[5];
+			Console.WriteLine();
+			Console.WriteLine(" ====================== Step 6 ====================== ");
+			Console.WriteLine();
+
+			Console.Write("Random class: ");
+			var random = new Random();
+			random.NextBytes(numbers);
+			PrintValues(numbers);
+			Console.WriteLine();
+
+			Console.Write("RNGCryptoServiceProvider: ");
+			var crypto = new RNGCryptoServiceProvider();
+			crypto.GetBytes(numbers);
+			PrintValues(numbers);
+			Console.WriteLine();
+		}
+
+		private static void Step4(byte[] secret, DPAPI dpapi)
+		{
+			Console.WriteLine();
+			Console.WriteLine(" ====================== Step 4 ====================== ");
+			Console.WriteLine();
+
+			//Encrypt the data. 
+			byte[] encryptedSecret = dpapi.Protect(secret, s_additionalEntropy);
+			Console.WriteLine("The encrypted byte array is:");
+			PrintValues(encryptedSecret);
+
+			// Decrypt the data and store in a byte array. 
+			byte[] originalData = dpapi.Unprotect(encryptedSecret, s_additionalEntropy);
+			Console.WriteLine("{0}The original data is:", Environment.NewLine);
+			PrintValues(originalData);
+		}
+
+		private static void Step5(byte[] secret, DPAPI dpapi)
+		{
+			Console.WriteLine();
+			Console.WriteLine(" ====================== Step 5 ====================== ");
+			Console.WriteLine();
+
+			string encryptedBase64 = dpapi.EncryptString(secret, s_additionalEntropy);
+			Console.WriteLine("Encrypted string: " + encryptedBase64);
+			Console.WriteLine();
+
+			byte[] originalBase64 = dpapi.DecryptString(encryptedBase64, s_additionalEntropy);
+			Console.WriteLine("Original data: ");
+			PrintValues(originalBase64);
+			Console.WriteLine();
+		}
+
+		public static void PrintValues(Byte[] myArr)
+		{
+			foreach (Byte i in myArr)
+			{
+				Console.Write("\t{0}", i);
+			}
+			Console.WriteLine();
+		}
+
+	}
+}
